9d81efdc6c128d3057b84554e5dfcba970c3a995
Cycyes <92714336+Cycyes@users.noreply.github.com>
2024-01-29 01:39:00 +0800

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -146,6 +146,16 @@ def dice_LPBA(gt, pred):
     return np.mean(dice_lst)
 
 
+def dice_AbdomenCTCT(outputs, labels, max_label):
+    dice = torch.FloatTensor(max_label - 1).fill_(0)
+    for label_num in range(1, max_label):
+        iflat = (outputs == label_num).view(-1).float()
+        tflat = (labels == label_num).view(-1).float()
+        intersection = torch.mean(iflat * tflat)
+        dice[label_num - 1] = (2. * intersection) / (1e-8 + torch.mean(iflat) + torch.mean(tflat))
+    return dice
+
+
 def jacobian_determinant_vxm(disp):
     """
     jacobian determinant of a displacement field.
